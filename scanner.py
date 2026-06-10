66c0d5c74ecd0858e9bf3579f98e5c4e66a841c3
Himanshu280487 <himanshudrockinguy@gmail.com>
2026-06-10 16:18:53 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,7 @@
-print("DOW THEORY SCANNER RUNNING")
+import pandas as pd
+
+url = "https://archives.nseindia.com/content/equities/EQUITY_L.csv"
+
+df = pd.read_csv(url)
+
+print(f"Total NSE stocks loaded: {len(df)}")

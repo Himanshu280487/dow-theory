305c523f06ba97c1c0b00d39fdd0544e6f876145
Himanshu280487 <himanshudrockinguy@gmail.com>
2026-06-10 16:21:31 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -9,6 +9,26 @@ df = yf.download(
     progress=False
 )
 
-print(symbol)
-print(f"Rows downloaded: {len(df)}")
-print(df.tail())
+highs = df["High"].tail(60)
+lows = df["Low"].tail(60)
+
+recent_high = highs[-20:].max()
+previous_high = highs[-40:-20].max()
+
+recent_low = lows[-20:].min()
+previous_low = lows[-40:-20].min()
+
+print(f"Recent High: {recent_high}")
+print(f"Previous High: {previous_high}")
+
+print(f"Recent Low: {recent_low}")
+print(f"Previous Low: {previous_low}")
+
+if recent_high > previous_high and recent_low > previous_low:
+    print("UPTREND")
+
+elif recent_high < previous_high and recent_low < previous_low:
+    print("DOWNTREND")
+
+else:
+    print("SIDEWAYS")
